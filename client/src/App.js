3dd6aba53bf1170fa259cf4577f7a6a1f037e433
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-02-25 01:07:48 +0200

    changes location of client folder

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/products" element={<ProductScreen />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+export default App;
