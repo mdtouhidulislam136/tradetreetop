e6c0436e95ccf20f5efc0919faf428edca819c2f
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-04-23 15:15:06 +0300

    Register user Route

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 import connectToDatabase from "./database.js";
 import express from "express";
 
-
 // our routes
 import productRoutes from "./routes/productRoutes.js";
-
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectToDatabase();
@@ -14,9 +13,9 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server runs on port ${port}.`);
 });
-
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,7 @@ import User from "../models/User";
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 
-const router = express.Router();
+const userRouter = express.Router();
 // redefine expiresIn
 
 const genToken = (id) => {
@@ -27,3 +27,37 @@ const logInUser = asyncHandler(async (req, res) => {
   }
   throw new Error("Invalid email or password");
 });
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await user.findOne({ tmail });
+  if (userExist) {
+    res.status(404);
+    throw new Error("We already have an account with that email address. ");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user_id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id),
+    });
+  } else {
+    res.json(400);
+    throw new Error("Invalied user data");
+  }
+});
+
+userRoutes.route("/login").post(logInUser);
+userRoutes.route("/register").post(registerUser);
+
+export default userRoutes;
