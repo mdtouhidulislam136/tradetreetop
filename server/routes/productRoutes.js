f3c7e966c15b89f1f5e4bd5b6d5cbd2480678a75
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-05-03 02:41:31 +0300

    Middleware file rename middleware

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,7 @@ import Product from "../models/Product.js";
 import asyncHandler from "express-async-handler";
 import { protectRoute, admin } from "../middleware/authMiddleware.js";
 
+
 const productRoutes = express.Router();
 const getProducts = async (req, res) => {
   const products = await Product.find({});
